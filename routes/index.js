285df982949bc60b7000c64f3eb0ffa6a1915165
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2022-12-18 21:40:41 +0530

    added another router for users profile

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const homeController = require("../controllers/home_controller");
 
 console.log("Router loaded");
 
-router.get("/",homeController.home);
+router.get("/" , homeController.home);
+router.use("/users" , require("./users"));
+// for any further routes, access from here
+// router.use("/routerName" , require("./routerfile"));
 
 module.exports = router;
