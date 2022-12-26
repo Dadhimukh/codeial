68065f3f2f04d4b4b08d7fdb8651a5a784bde699
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2022-12-26 12:37:14 +0530

    we created singIn page and singUp page and connect to routes folder in user.js file and simply in controllers folder in user_controller file also

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,19 @@
 module.exports.profile = function(req,res){
     return res.render("user_profile" , {title:"User Profile"});
 }
+
+
+// render the sing up page
+module.exports.signUp = function(req , res){
+    return res.render("user_sign_up" , {
+        title : "codeial | Sign Up"
+    });
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req , res){
+    return res.render("user_sign_in" , {
+        title: "codeial | Sign In"
+    });
+}
