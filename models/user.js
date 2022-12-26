9165f52f8a9612d7850b7c4f37c27a4e7c46a463
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2022-12-26 15:04:46 +0530

    we created Sign up page for user as well as storing data in DataBase

--- models/user.js
+++ models/user.js
@@ -18,6 +18,6 @@ const userSchema = new mongoose.Schema({
     timestamps:true
 });
 
-const user = mongoose.model("User" , userSchema);
+const User = mongoose.model("User" , userSchema);
 
 module.exports = User;
