58bbc450a30da02d2a90d3c10fbb82fde52d721a
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-05 11:53:47 +0530

    displaying posts on web home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,20 @@
+const Post = require("../models/post");
+
 module.exports.home = function(req,res){
     // return res.end("<h1> Express is up for codeial !</h1>");
 
-    console.log(req.cookies);
+    // console.log(req.cookies);
 
     // changing the value of cookies in response
-    res.cookie("user_id" , 25);
+    // res.cookie("user_id" , 25);
 
-    return res.render("home" , {
-        title:"Home"
+    Post.find({} , function(err , posts){
+
+        return res.render("home" , {
+            title:"Codeial | Home",
+            posts   :   posts
+        });
+    
     });
+
 }
