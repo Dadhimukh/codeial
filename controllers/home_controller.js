999aea07c239b359c357fe8093c24ceda53a22af
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-05 14:03:56 +0530

    Nested population===> and ====> Displaying the comments of user and the user email id

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -19,7 +19,16 @@ module.exports.home = function(req,res){
     
 
 // Populate the user of each post
-    Post.find({}).populate("user").exec(function(err , posts){
+    Post.find({})
+    .populate("user")
+    .populate({
+        path :  "comments",
+        populate :  {
+            path :  "user"
+        }
+
+    })
+    .exec(function(err , posts){
 
         return res.render("home" , {
             title:"Codeial | Home",
