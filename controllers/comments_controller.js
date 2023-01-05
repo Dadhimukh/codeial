2ba31b778b5f00edfb999287aa650bdc0f6581e9
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-05 13:43:49 +0530

    Adding Comments to Database

---
+++ controllers/comments_controller.js
@@ -0,0 +1,24 @@
+const Comment = require("../models/comment");
+const Post = require("../models/post");
+
+module.exports.create = function(req , res){
+    Post.findById(req.body.post , function(err , post){
+        if (post){
+            Comment.create({
+                content :   req.body.content,
+                post :  req.body.post,
+                user :  req.user._id
+
+            }, function(err , comment){
+                if(err){console.log("error to creating comments")}
+
+                post.comments.push(comment);
+                post.save();
+
+                res.redirect("/");
+
+            })
+        }
+    })
+
+}
