e342b71a25c683b45d0c84b4d8892feaa1a586be
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-06 13:33:04 +0530

    Updateing the user's profile name

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,17 @@ module.exports.profile = function(req,res){
 }
 
 
+module.exports.update = function(req , res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id , req.body , function(err , user){
+            return res.redirect("back");
+        });
+    }else{
+        return res.status(401).send("Unauthorized");
+    }
+}
+
+
 // render the sing up page
 module.exports.signUp = function(req , res){
 
