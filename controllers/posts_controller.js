06e8126f5c3900835413b71294901c67f56845b1
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-07 21:53:27 +0530

    adding more flash massages to user

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -9,11 +9,14 @@ module.exports.create = async function (req, res) {
         user: req.user._id,
       });
 
+      req.flash("success" , "Post Published!")
     return res.redirect("back");
 
   } catch (err) {
-    console.log("Error", err);
-    return;
+    
+    req.flash("error" , err);
+    return res.redirect("back");
+
   }
 }
 
@@ -24,13 +27,21 @@ module.exports.destroy = async function (req, res) {
     if (post.user == req.user.id) {
       post.remove();
       await Comment.deleteMany({ post: req.params.id });
+
+      req.flash("success" , "Post and associated comments deleted");
+
       return res.redirect('back');
     } else {
+
+      req.flash("error" , "You cannot delet this post");
+
       return res.redirect('back');
     }
 
   } catch (err) {
-    console.log("Error", err);
-    return;
+
+    req.flash("error" , err);
+    return res.redirect("back");
+
   }
 }
