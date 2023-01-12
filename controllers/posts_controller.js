fffa69740f47741a050855ac26f5f308cab2513b
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-12 16:23:08 +0530

    COnverting into AJAX ===> Creating Post and Sending Data

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,11 +3,20 @@ const Comment = require("../models/comment");
 
 module.exports.create = async function (req, res) {
   try {
-    await Post.create(
+    let post = await Post.create(
       {
         content: req.body.content,
         user: req.user._id,
       });
+
+      if (req.xhr){
+        return res.status(200).json({
+          data :  {
+            post :  post
+          },
+          message :   "Post created!"
+        });
+      }
 
       req.flash("success" , "Post Published!")
     return res.redirect("back");
