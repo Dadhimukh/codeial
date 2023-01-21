5153942e892b3e82c62ad148eecd57861c4d7836
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-21 10:53:33 +0530

    converting to ajax :: deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -37,6 +37,15 @@ module.exports.destroy = async function (req, res) {
       post.remove();
       await Comment.deleteMany({ post: req.params.id });
 
+      if(req.xhr){
+        return res.status(200).json({
+          data: {
+            post_id:  req.params.id
+          },
+          message:  "Post deleted successfully"
+        });
+      }
+
       req.flash("success" , "Post and associated comments deleted");
 
       return res.redirect('back');
