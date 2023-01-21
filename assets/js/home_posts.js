d6f84b30058a361d332cb7f1cec7d2234a6c452c
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-01-21 10:04:42 +0530

    Create a post :: Receiving Data without reloading the page

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -3,16 +3,17 @@
       let createPost = function () {
             let newPostForm = $("#new-post-form");
 
-            newPostForm.submit(function(e){
+            newPostForm.submit(function (e) {
                   e.preventDefault();
 
                   $.ajax({
                         type: "post",
-                        url:  '/posts/create',
+                        url: '/posts/create',
                         data: newPostForm.serialize(),
-                        success:    function(data){
-                              console.log(data);
-                        },error:    function(error){
+                        success: function (data) {
+                              let newPost = newPostDom(data.data.post);
+                              $('#posts-list-container>ul').prepend(newPost);
+                        }, error: function (error) {
                               console.log(error.responseText);
                         }
                   });
@@ -20,7 +21,41 @@
       }
 
       // method to create a post in DOM
+      let newPostDom = function (post) {
+            return $(`<li id="post-${post._id}">
+            <p>
+            
+                  <small>
+                        <a class="delete-post-button" href="/posts/destroy/${post.id}">Delete</a>
+                  </small>
 
+                  ${ post.content }
+                  <br />
+                  <small>
+                        ${ post.user.name }
+                  </small>
+            </p>
+      
+            <div class="post-comments">
+      
+                        <form action="/comments/create"     method="POST">
+                              <input type="text" name="content" placeholder="Type Here To add comment...." required />
+            
+                              <input type="hidden" name="post" value="${ post._id }" />
+            
+                              <input type="submit" value="Add Comment" />
+                        </form>
+      
+                  <div class="post-comments-list">
+                        <ul id="post-comments-${ post._id }">
+
+                        </ul>
+                  </div>
+      
+      
+            </div>
+            </li>`)
+      }
 
       createPost();
 
